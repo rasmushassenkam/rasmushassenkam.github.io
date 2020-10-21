17cdd1745063151d881b9022809e2fc3f8832c4a
Rasmus <rhp@plantjammer.com>
2020-10-21 13:55:45 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Find all widget divs
+const WidgetDivs = document.querySelectorAll(".reddit_widget");
+
+// Inject our React App into each
+WidgetDivs.forEach((Div) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App domElement={Div} />
+    </React.StrictMode>,
+    Div
+  );
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
